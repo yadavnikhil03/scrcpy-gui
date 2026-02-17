dbba2b12860620c01295922a8a43d8fe444e31a8
KB @ kilObit <106017926+kil0bit-kb@users.noreply.github.com>
2026-02-17 11:28:53 +0545

    ci: fix reqwest version, remove opener plugin, and simplify build args

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,7 +11,6 @@ pub struct ScrcpyState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
